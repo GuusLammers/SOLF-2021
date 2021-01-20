7d6b30b46c7f239e402e8843f919009d730bed16
GuusLammers <guuslammers16@gmail.com>
2021-01-19 16:28:08 -0800

    initial commit

---
+++ SOLF 2021/Assets/Scripts/ControlPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPoint : MonoBehaviour
+{
+    float xRotation, yRotation = 0f;
+
+    public Rigidbody ball;
+
+    public float rotationSpeed = 5f;
+
+    public float shootPower = 30f;
+
+    void Update()
+    {
+        transform.position = ball.position;
+
+        if (Input.GetMouseButton(0))
+        {
+            xRotation += Input.GetAxis("Mouse X") * rotationSpeed;
+            yRotation += Input.GetAxis("Mouse Y") * rotationSpeed;
+
+            if (yRotation < -35f)
+            {
+                yRotation = -35f;
+            }
+
+            transform.rotation = Quaternion.Euler(yRotation, xRotation, 0f);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ball.velocity = transform.forward * shootPower;
+        }
+    }
+}
